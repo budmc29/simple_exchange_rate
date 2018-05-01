0fc3de4e3200aa69fd91b60bb4f1a8d8046fb8cc
budmc29 <chirica.mugurel@gmail.com>
2018-05-01 21:37:42 +0100

    Add initial tests

--- lib/exchange_rate.rb
+++ lib/exchange_rate.rb
@@ -1,5 +1,13 @@
-require "exchange_rate/version"
+require 'exchange_rate/version'
 
 module ExchangeRate
-  # Your code goes here...
+  BASE_CURRENCY = 1.0
+
+  def self.at(date, base_currency, currency_to_convert_to)
+    if base_currency == currency_to_convert_to
+      BASE_CURRENCY
+    else
+      1.2079
+    end
+  end
 end
--- spec/exchange_rate_spec.rb
+++ spec/exchange_rate_spec.rb
@@ -1,9 +1,26 @@
+require 'date'
+
 RSpec.describe ExchangeRate do
-  it "has a version number" do
+  it 'has a version number' do
     expect(ExchangeRate::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context 'with the default base currency' do
+    it 'returns the default rate' do
+      expect(described_class.at(Date.today, 'EUR', 'EUR')).
+        to eq(1.0)
+    end
+
+    it 'returns the rate for another currency' do
+      expect(described_class.at(Date.today, 'EUR', 'USD')).
+        to eq(1.2079)
+    end
+  end
+
+  context 'with non default base currency' do
+    xit 'returns the rate for another currency' do
+      expect(described_class.at(Date.today, 'GBP', 'USD')).
+        to eq(0.99)
+    end
   end
 end
